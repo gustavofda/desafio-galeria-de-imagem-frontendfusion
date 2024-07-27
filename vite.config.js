021fb9cc6d84a4f14f92a49d38a7cd30610d1976
gustavofda <gustavoaraujo2017fise@gmil.com>
2024-07-26 21:56:02 -0300

    Alterações

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: {
-      '.js': 'jsx', 
-      '.ts': 'tsx',  
-      '.tsx': 'tsx', 
-    },
-  },
+  
 });
